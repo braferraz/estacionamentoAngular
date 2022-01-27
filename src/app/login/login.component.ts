fc1103096625de4ce95d572b4a9b2b1807838225
Braian Ferraz <braianfs2009@gmail.com>
2022-01-27 07:47:03 -0300

    Tela de Login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  loginForm = this.formBuilder.group({
+    usuario: ["", String, Validators.required, Validators.minLength(3)],
+    senha: ["", String,  Validators.required, Validators.minLength(5)]
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.warn(this.loginForm.value);
+  }
+}
